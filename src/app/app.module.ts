2e81b1b59c27989c7b5b8fa6dc05d0f96f02328f
Wojciech Rzoska <wojciechrzoska@gmail.com>
2025-01-28 23:09:31 +0100

    manage folder strucutre, db module and user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from 'src/modules/user/user.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
