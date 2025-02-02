371a5363aac04b0ce8243bc985ff65c5bd74dc35
Wojciech Rzoska <wojciechrzoska@gmail.com>
2025-02-02 15:20:26 +0100

    add husky

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from 'src/modules/user/user.module';
+
+import { UserModule } from 'src/user/user.module';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [DatabaseModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
